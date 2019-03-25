f4e190c955c148e67ae6de4efeca70c4adc7c5a0
Ray Miru <raymiru.one@gmail.com>
2019-03-25 22:49:50 +0700

    add players page and update players array, add props to match and players components

--- client/src/views/Players.vue
+++ client/src/views/Players.vue
@@ -7,13 +7,21 @@
                         <v-data-table
                                 :headers="headers"
                                 :items="players"
+                                :footer="players"
                                 class="elevation-1"
                         >
                             <template v-slot:items="props">
                                 <td>{{ props.item.player_id }}</td>
                                 <td>{{ props.item.steam_username }}</td>
                                 <td>{{ props.item.bank }}</td>
+                                <td>{{ props.item.bank }}</td>
+                                <td>{{ props.item.bank }}</td>
+                                <td>{{ props.item.bank }}</td>
 
+                            </template>
+                            <template v-slot:footer>
+                                <td></td><td></td><td></td><td></td><td></td>
+                                <td style="background-color: blanchedalmond;">{{summary}}</td>
                             </template>
                         </v-data-table>
                     </v-flex>
@@ -32,19 +40,37 @@
                 headers: [
                     {text: 'ID', value: 'player_id'},
                     {text: 'Steam Username', value: 'steam_username'},
+                    {text: 'Bet', value: 'bank'},
+                    {text: 'Odds', value: 'bank'},
+                    {text: 'P.Win', value: 'bank'},
                     {text: 'Bank', value: 'bank'},
+                ],
 
-                ]
             }
 
         },
         props: {
             players: Array
         },
-        methods: {
-
+        methods: {},
+        computed: {
+            summary: function () {
+                let summ = 0;
+                this.players.forEach(elem => {
+                    summ += parseFloat(elem.bank)
+                });
+                return summ
+            }
         }
     }
+
+            // players: function (data) {
+            //     data.forEach(elem => {
+            //         this.summary += parseFloat(elem.bank)
+            //     })
+            // }
+
+
 </script>
 
 <style scoped>
